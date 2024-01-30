aa2f7cd465a7483f670b6e51346b7f99f45f5dbb
Nicole Uchoa <nicolle-uchoa@hotmail.com>
2024-01-30 17:26:28 -0500

    filtragem

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import { useState } from 'react';
 import './App.css'
 import { List } from './components/list/list';
@@ -9,22 +6,41 @@ import { CreateModal } from './components/create-modal/create-modal';
 
 function App() {
   // guardar os dados da api 
-
-  const { data } = useUsersData();
+  const [filtro, setFiltro] = useState('');
+  const { data } = useUsersData(filtro);
   const [isModalOpen, setIsModalOpen] = useState(false)
 
   const handleOpenModal = () => {
     setIsModalOpen(prev => !prev)
   }
 
+  const handleLimparFiltro = () => {
+    setFiltro('');
+  };
 
   return (
     <div className='container'>
       <h1>Usuários</h1>
 
-      <div className='lista'>
-        {data?.map(userData => <List name={userData.name} />)} 
+      <div>
+        <input
+          type='text'
+          placeholder='Filtrar por nome'
+          value={filtro}
+          onChange={(e) => setFiltro(e.target.value)}
+        />
+        <button onClick={handleLimparFiltro}>Limpar Filtro</button>
       </div>
+
+      <div className='lista'>
+        {data?.map((userData) => (
+          <List key={userData.id} name={userData.name} />
+        ))}
+      </div>
+
+      {/* <div className='lista'>
+        {data?.map(userData => <List name={userData.name} />)} 
+      </div> */}
       {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
       <button onClick={handleOpenModal}>Cadastrar novo usuário</button>
       
--- src/hooks/useUsersDataGet.ts
+++ src/hooks/useUsersDataGet.ts
@@ -4,22 +4,25 @@ import { useQuery } from "@tanstack/react-query";
 
 const API_URL = "http://localhost:8080/usuarios"
 
-const fetchData =async (): AxiosPromise<UserData[]> => {
-    const response = axios.get(API_URL);
+const fetchData = async (filtro: string): AxiosPromise<UserData[]> => {
+    console.log('Valor do filtro:', filtro);
+    const response = axios.get(`${API_URL}?name=${filtro}`);
+    console.log('Resposta da API:', (await response).data);
     return response;
-    
+
 }
 
-export function useUsersData(){
+export function useUsersData(filtro: string) {
+    console.log("filtro", filtro)
     const query = useQuery({
-        queryFn: fetchData,
-        queryKey: ['user-data'],
+        queryFn: () => fetchData(filtro),
+        queryKey: ['user-data', filtro],
         retry: 2
-    })
-
+    }
+    );
 
     return {
-        ...query ,
-        data: query.data?.data
-    }
+        ...query,
+        data: query.data?.data,
+    };
 }
